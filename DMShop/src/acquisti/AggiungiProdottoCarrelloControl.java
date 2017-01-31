550721354b0645785a886023b5d28e7c53c389bf
SalJavi4 <salvatore.95_@live.it>
2017-01-31 20:02:10 +0100

    Implementati control nel package acquisti
    Implementati AggiungiProdottoCarrelloControl e
    RimuoviProdottoCarrelloControl

--- DMShop/src/acquisti/AggiungiProdottoCarrelloControl.java
+++ DMShop/src/acquisti/AggiungiProdottoCarrelloControl.java
@@ -7,13 +7,30 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 @WebServlet("/AggiungiProdottoCarrelloControl")
 public class AggiungiProdottoCarrelloControl extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
+		HttpSession session = request.getSession(true);
+		int idProdotto = Integer.parseInt(request.getParameter("idProdotto"));
+		int quantita = Integer.parseInt(request.getParameter("quantita"));
+		
+		Carrello carrello = (Carrello) session.getAttribute("carrello");
+		if (carrello != null)
+		{
+			carrello.aggiungiProdottoNelCarrello(idProdotto, quantita);
+		}
+		else
+		{
+			carrello = new Carrello();
+			carrello.aggiungiProdottoNelCarrello(idProdotto, quantita);
+			session.setAttribute("carrello", carrello);
+		}
 		
 	}
 
--- DMShop/src/acquisti/RimuoviProdottoCarrelloControl.java
+++ DMShop/src/acquisti/RimuoviProdottoCarrelloControl.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/RimuoviProdottoCarrelloControl")
 public class RimuoviProdottoCarrelloControl extends HttpServlet {
@@ -13,6 +14,15 @@ public class RimuoviProdottoCarrelloControl extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		HttpSession session = request.getSession(true);
+		int idProdotto = Integer.parseInt(request.getParameter("idProdotto"));
+		
+		Carrello carrello = (Carrello) session.getAttribute("carrello");
+		if (carrello != null)
+		{
+			carrello.rimuoviProdottoDalCarrello(idProdotto);
+		}
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
