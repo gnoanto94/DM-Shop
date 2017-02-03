0d671b51f9cc4ca8a8f29c75d41ab34da4302b22
SalJavi4 <salvatore.95_@live.it>
2017-02-03 01:43:28 +0100

    Creata e implementata VisualizzaDettagliOrdineControl.java

--- DMShop/src/ordini/VisualizzaDettagliOrdineControl.java
+++ DMShop/src/ordini/VisualizzaDettagliOrdineControl.java
@@ -1,40 +1,42 @@
 package ordini;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class VisualizzaDettagliOrdineControl
- */
+import ordini.GestioneOrdini;
+import ordini.Ordine;
+
 @WebServlet("/VisualizzaDettagliOrdineControl")
 public class VisualizzaDettagliOrdineControl extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public VisualizzaDettagliOrdineControl() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+    private static final Logger logger = Logger.getLogger("logger");
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		int idVisualizzaOrdine = Integer.parseInt(request.getParameter("idVisualizzaOrdine"));
+		logger.info("Valore di idVisualizzaOrdine= "+idVisualizzaOrdine);
+		Ordine o = GestioneOrdini.ricercaOrdine(idVisualizzaOrdine);
+		ArrayList<DettagliOrdine> ordini = o.getDettagli();
+		
+		if (ordini != null)
+		{
+			request.setAttribute("ordini", ordini);
+			RequestDispatcher dispatcher = request.getRequestDispatcher("ordine.jsp");
+			dispatcher.forward(request, response);
+		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
 		doGet(request, response);
 	}
 
