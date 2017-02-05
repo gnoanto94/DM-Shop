15d4c10bd46ff2b56d7389cadc3f074c5cb64318
gnoanto94 <gnoanto94@gmail.com>
2017-02-05 22:05:36 +0100

    Implementato GestioneOrdiniTest

--- DMShop/src/testing/GestioneOrdiniTest.java
+++ DMShop/src/testing/GestioneOrdiniTest.java
@@ -2,63 +2,60 @@ package testing;
 
 import static org.junit.Assert.*;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import ordini.DettagliOrdine;
+import ordini.GestioneOrdini;
+import ordini.Ordine;
+import utenti.GestioneUtenti;
+
 public class GestioneOrdiniTest {
+	
+	private ArrayList<Ordine> ordini;
+	private Ordine ordine;
 
 	@Before
 	public void setUp() throws Exception {
+		ordini = new ArrayList<>();
+		//si inserisce un ordine al gestore per prova
+		ordine = new Ordine(new Timestamp(System.currentTimeMillis()), GestioneUtenti.ricercaUtentePerId(1), new ArrayList<DettagliOrdine>(), 0.00	, 1);
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		ordini = null;
+		ordine = null;
 	}
 
 	@Test
 	public void testGetOrdini() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testToString() {
-		fail("Not yet implemented");
+		ordini = GestioneOrdini.getOrdini();
+		assertNotNull(ordini);
 	}
 
 	@Test
 	public void testAggiungiOrdine() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testRimuoviOrdine() {
-		fail("Not yet implemented");
+		boolean inserito = GestioneOrdini.aggiungiOrdine(ordine);
+		assertTrue(inserito);
 	}
 
 	@Test
 	public void testFiltraOrdiniPerUtente() {
-		fail("Not yet implemented");
+		//si verificano gli ordini del gestore per prova
+		ArrayList<Ordine> ordiniUtente = GestioneOrdini.filtraOrdiniPerUtente(1);
+		assertNotNull(ordiniUtente);
 	}
 
 	@Test
 	public void testFiltraOrdiniPerStato() {
-		fail("Not yet implemented");
+		ArrayList<Ordine> ordiniNuovi = GestioneOrdini.filtraOrdiniPerStato(1); //ordini nuovi
+		assertNotNull(ordiniNuovi);
 	}
 
-	@Test
-	public void testRicercaOrdine() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testModificaStatoOrdine() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testImportaOrdini() {
-		fail("Not yet implemented");
-	}
 
 }
