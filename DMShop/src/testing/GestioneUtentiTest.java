4bdd64974ee824adc8c11c0a71acc73ca4db324a
gnoanto94 <gnoanto94@gmail.com>
2017-02-05 21:50:12 +0100

    Implementato GestioneUtentiTest

--- DMShop/src/testing/GestioneUtentiTest.java
+++ DMShop/src/testing/GestioneUtentiTest.java
@@ -2,58 +2,71 @@ package testing;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import utenti.GestioneUtenti;
+import utenti.Utente;
+
 public class GestioneUtentiTest {
+	
+	Utente utente;
+	ArrayList<Utente> utenti;
 
 	@Before
 	public void setUp() throws Exception {
+		utente = new Utente("nomeProva", "cognomeProva", "email@prova.it", "P@ssw0rdProva", "indirizzoProva", "cittaProva", "Pr", "111111111");
+		utenti = new ArrayList<>();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		utente = null;
+		utenti = null;
 	}
 
 	@Test
+	public void testAll(){ //per questa classe è necessario testare i metodi in ordine
+		testGetUtenti();
+		testVerificaDisponibilitaEmail();
+		testAggiungiUtente();
+		testVerificaCredenziali();
+		testRicercaUtentePerId();
+		testRimuoviUtente();
+	}
+	
 	public void testGetUtenti() {
-		fail("Not yet implemented");
+		utenti = GestioneUtenti.getUtenti();
+		assertNotNull(utenti);
 	}
-
-	@Test
-	public void testToString() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testVerificaCredenziali() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testRicercaUtentePerId() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAggiungiUtente() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testRimuoviUtente() {
-		fail("Not yet implemented");
-	}
-
-	@Test
+	
 	public void testVerificaDisponibilitaEmail() {
-		fail("Not yet implemented");
+		boolean disponibile = GestioneUtenti.verificaDisponibilitaEmail(utente.getEmail());
+		assertTrue(disponibile);
 	}
 
-	@Test
-	public void testImportaUtenti() {
-		fail("Not yet implemented");
+	public void testAggiungiUtente() {
+		boolean inserito = GestioneUtenti.aggiungiUtente(utente);
+		assertTrue(inserito);
+	}
+	
+	public void testVerificaCredenziali() {
+		Utente u = GestioneUtenti.verificaCredenziali("email@prova.it", "P@ssw0rdProva");
+		assertNotNull(u);
+	}
+
+	public void testRicercaUtentePerId() {
+		Utente u = GestioneUtenti.verificaCredenziali("email@prova.it", "P@ssw0rdProva");
+		Utente trovato = GestioneUtenti.ricercaUtentePerId(u.getId());
+		assertEquals(utente, trovato);
+	}
+
+	public void testRimuoviUtente() {
+		boolean rimosso = GestioneUtenti.rimuoviUtente(utente);
+		assertTrue(rimosso);
 	}
 
 }
